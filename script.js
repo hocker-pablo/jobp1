7243369058368fabb1271327320d6ee995ae7206
hocker.pablo <shinra.sh.mlg@gmail.com>
2023-09-16 20:58:17 -0300

    main

---
+++ script.js
@@ -0,0 +1,7 @@
+let tirarPrompt = window.prompt("por favor ingresa tu edad");
+
+if(tirarPrompt !== null ){
+    alert("hola, " + tirarPrompt + " !bienvenido")
+} else {
+    alert("no cumples con la edad requerida")
+}
