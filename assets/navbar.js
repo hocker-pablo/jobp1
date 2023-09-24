c3ccf470770cac802ad0f10aa74feb3a63724782
hocker.pablo <shinra.sh.mlg>
2023-09-24 20:11:21 -0300

    master

---
+++ assets/navbar.js
@@ -0,0 +1,13 @@
+let lista = document.getElementsByClassName('opcion');
+let submenu = document.getElementsByClassName("submenu")
+
+lista = [...lista]
+submenu = [...submenu]
+
+list.forEach(item => {
+    item.addEventListener('click', (i) =>{
+       let elemento = i.target.document.children
+    })
+});
+
+
